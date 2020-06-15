e548c638195dff16145f58a2a35e518c5df2de7c
obyrneb <byron.obyrne@gmail.com>
2020-06-15 13:45:41 -0400

    now including plot

--- analysis.R
+++ analysis.R
@@ -22,10 +22,13 @@ ol_aug <- ol_raw %>%
                               region_e == "West" ~ "English",
                               region_e == "NCR" ~ "Bilingual",
                               region_e == "Quebec except NCR" ~ "French"),
+         my_dept = case_when(requesting_department_e == "Treasury Board (Secretariat)" ~ "TBS",
+                             TRUE ~ "Other"),
          n_pass = (pass_rate_e / 100 * total_number_tests) %>% round(),
          n_fail = total_number_tests - n_pass) %>%
   select(fiscal_year,
          dept = requesting_department_e,
+         my_dept,
          region = region_e,
          test_language,
          test_level,
@@ -33,7 +36,15 @@ ol_aug <- ol_raw %>%
          pass_rate = pass_rate_e,
          n_tests = total_number_tests,
          n_pass,
-         n_fail) %>% 
-  filter(fiscal_year == "2018-2019") %>% 
-  left_join(pop %>% select(dept, fol_en, fol_fr)) 
+         n_fail) #%>% 
+  #filter(fiscal_year == "2018-2019") %>% 
+  #left_join(pop %>% select(dept, fol_en, fol_fr)) 
 
+ggplot(ol_aug) +
+  geom_point(aes(x = fiscal_year, y = pass_rate, size = n_tests, fill = region, alpha = my_dept, colour = my_dept),
+             position = position_jitter(width = 0.1, height = 0),
+             shape = 21) +
+  scale_alpha_manual(values = c(TBS = 1, Other = 0.2)) +
+  scale_colour_manual(values = c(TBS = "black", Other = "grey80")) +
+  #geom_smooth(aes(x = fiscal_year, y = pass_rate)) +
+  facet_grid(rows = vars(test_language), cols = vars(test_level))
